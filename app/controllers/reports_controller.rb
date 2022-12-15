662c3177abc978b6c6484b742982d62c05ead10c
Jack Collins <jackpaulcollins@gmail.com>
2022-12-14 19:48:00 -0800

    turn off slack

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -41,7 +41,7 @@ class ReportsController < ApplicationController
 
     respond_to do |format|
       if @report.save
-        post_to_slack(@report)
+        #post_to_slack(@report)
         format.html { redirect_to @report, notice: "Report was successfully created." }
         format.json { render :show, status: :created, location: @report }
       else
