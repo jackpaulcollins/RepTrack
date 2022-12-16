5aa9e39e396b4e4c9b68492f10366ec18ebc10a5
Jack Collins <jackpaulcollins@gmail.com>
2022-12-15 21:14:40 -0800

    bug

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,8 @@ Sidekiq.configure_server do |config|
   config.logger = Sidekiq::Logger.new($stdout)
 end
 
-Sidekiq.configure_client do |config|
-  config.redis = ENV["REDIS_URL"]
+if Rails.env.production?
+  Sidekiq.configure_client do |config|
+    config.redis = { host: ENV["REDIS_URL"] }
+  end
 end
