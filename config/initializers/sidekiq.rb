c52db8b3ad6cb99f5156589f8fa86ee9fa744509
Jack Collins <jackpaulcollins@gmail.com>
2022-12-15 21:08:43 -0800

    sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,11 @@
 Sidekiq.configure_server do |config|
   config.logger = Sidekiq::Logger.new($stdout)
 end
+
+Sidekiq.configure_client do |config|
+  config.redis = ENV["REDIS_URL"]
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = redis
+end
