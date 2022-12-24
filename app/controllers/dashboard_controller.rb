242e8710efc57ef445d9eb66a6db85d91babb1f2
Jack Collins <jackpaulcollins@gmail.com>
2022-12-24 10:16:11 -0800

    scope users for dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def show
-    users = User.all
+    users = current_account.users
     leader_board = users.sort_by { |u| u.reports.sum(:points)}
     @leader_board = leader_board.reverse
   end
